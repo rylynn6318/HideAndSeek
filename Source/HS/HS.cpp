335039146424617bc1894c50c79d0142be3cc523
donghyeon <gg6318@naver.com>
2023-05-22 23:31:12 +0900

    Initial commit

---
+++ Source/HS/HS.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HS, "HS" );
+ 
---
+++ Source/HS/HSGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HSGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AHSGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AHSGameMode();
+};
+
+
+
