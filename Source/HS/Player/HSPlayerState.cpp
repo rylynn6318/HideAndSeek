68326fd206b590d695b4097792305ed8b138f3f0
donghyeon <gg6318@naver.com>
2023-05-25 23:09:39 +0900

    PlayerState 추가

---
+++ Source/HS/Player/HSPlayerState.cpp
@@ -0,0 +1,20 @@
+﻿#include "HSPlayerState.h"
+#include "AbilitySystemComponent.h"
+#include "AbilitySystem/HSAbilitySystemComponent.h"
+
+AHSPlayerState::AHSPlayerState()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	AbilitySystemComponent = CreateDefaultSubobject<UHSAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+}
+
+UAbilitySystemComponent* AHSPlayerState::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+UHSAbilitySystemComponent* AHSPlayerState::GetHSAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
---
+++ Source/HS/Player/HSPlayerState.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AbilitySystemInterface.h"
+#include "HSPlayerState.generated.h"
+
+class UAbilitySystemComponent;
+class UHSAbilitySystemComponent;
+
+UCLASS()
+class HS_API AHSPlayerState : public APlayerState, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+
+public:
+	AHSPlayerState();
+
+	// IAbilitySystemInterface
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	// ~IAbilitySystemInterface
+	
+	virtual UHSAbilitySystemComponent* GetHSAbilitySystemComponent() const;
+
+protected:
+	UPROPERTY()
+	TObjectPtr<UHSAbilitySystemComponent> AbilitySystemComponent;
+};
