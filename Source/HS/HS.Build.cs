c1f798d829add7956a3dce2ff08e38b8c916d704
donghyeon <gg6318@naver.com>
2023-05-30 23:07:45 +0900

    TargetingSystem 추가

--- Source/HS/HS.Build.cs
+++ Source/HS/HS.Build.cs
@@ -8,7 +8,7 @@ public class HS : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "GameplayTags", "GameplayTasks","Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "GameplayTags", "GameplayTasks", "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput", "TargetingSystem" });
 
 		PrivateIncludePaths.AddRange(new string[] { "HS" });
 	}
