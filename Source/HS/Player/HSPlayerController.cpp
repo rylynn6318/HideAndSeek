66a4ca1913492b95b82fc3d4c7b80805d02f2be0
donghyeon <gg6318@naver.com>
2023-06-08 00:10:50 +0900

    플레이어 컨트롤러 추가

---
+++ Source/HS/Player/HSPlayerController.cpp
@@ -0,0 +1,5 @@
+
+
+
+#include "Player/HSPlayerController.h"
+
---
+++ Source/HS/Player/HSPlayerController.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "HSPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HS_API AHSPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
