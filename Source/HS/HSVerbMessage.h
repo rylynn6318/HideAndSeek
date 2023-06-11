826dccabe343a0b1849a1e663f72a01cd1aefd31
donghyeon <gg6318@naver.com>
2023-06-11 22:21:04 +0900

    VerbMessage 구조체 추가

---
+++ Source/HS/HSVerbMessage.h
@@ -0,0 +1,33 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "HSVerbMessage.generated.h"
+
+USTRUCT(BlueprintType)
+struct FHSVerbMessage
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadWrite, Category = "Gameplay")
+	FGameplayTag Verb;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Gameplay")
+	TObjectPtr<UObject> Instigator = nullptr;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Gameplay")
+	TObjectPtr<UObject> Target = nullptr;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Gameplay")
+	FGameplayTagContainer InstigatorTags;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Gameplay")
+	FGameplayTagContainer TargetTags;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Gameplay")
+	FGameplayTagContainer ContextTags;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Gameplay")
+	double Magnitude = 1.0;
+};
