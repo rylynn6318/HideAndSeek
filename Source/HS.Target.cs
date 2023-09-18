b7f6d8d8913dd93cc9ed455a4165ca609e2145e3
donghyeon <gg6318@naver.com>
2023-09-19 00:12:49 +0900

    5.3 대응

--- Source/HS.Target.cs
+++ Source/HS.Target.cs
@@ -8,8 +8,8 @@ public class HSTarget : TargetRules
 	public HSTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_2;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("HS");
 	}
 }
--- Source/HSEditor.Target.cs
+++ Source/HSEditor.Target.cs
@@ -8,8 +8,8 @@ public class HSEditorTarget : TargetRules
 	public HSEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_2;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("HS");
 	}
 }
